19569cd317e98719c74ff53d3ff1fc6379ee4332
jebyrnes <jarrett.byrnes@umb.edu>
2024-02-12 11:49:59 -0500

    Initial commit

---
+++ scripts/2_make_interactions_tsv.R
@@ -0,0 +1,5 @@
+#' ------------------------------------------------------------------------------------
+#' Takes globi pieces and turns them into a valid
+#' interactions.tsv file for https://github.com/globalbioticinteractions/template-dataset
+#' ------------------------------------------------------------------------------------
+
